1551bff28f2f8af12ade23bb237b10d37eefa1e0
Sajid778 <sajidahmed7377@gmail.com>
2024-09-24 11:16:13 +0600

    faq section added

--- js/script.js
+++ js/script.js
@@ -142,9 +142,9 @@ document.getElementById('blog-btn').addEventListener('click',function(){
 });
 
 
-document.getElementById('blog-btn').addEventListener("click", function() {
-    window.location.href = "http://127.0.0.1:5500/blogs.html";
-  });
+// document.getElementById('blog-btn').addEventListener("click", function() {
+//     window.location.href = "http://127.0.0.1:5500/blogs.html";
+//   });
 
 
 
